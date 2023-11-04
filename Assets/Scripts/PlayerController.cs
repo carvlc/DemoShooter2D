02c27a58dbb68ab5740b8950dda517f47a751433
carvlc <cnsvilca@gmail.com>
2023-11-04 11:35:23 -0300

    New Issue: Player can fire laser gun

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,11 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
+using UnityEditor.ShaderKeywordFilter;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
     // se declara un atributo Player
-    [SerializeField] private Player player;
+    private Player player;
+    [SerializeField]private GameObject shuriken;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -24,5 +28,13 @@ public class PlayerController : MonoBehaviour
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
         player.Move(horizontalInput, verticalInput);
+
+        if (Input.GetButtonDown("Fire1"))
+        {
+            // es un metodo que permite instanciar gameObjects
+            Instantiate(shuriken, transform.position + new Vector3(0,1,0), quaternion.identity);
+        }
     }
+
+
 }
